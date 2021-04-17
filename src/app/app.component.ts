e6baaa46adc8edba2a1e130ac9967cbadc104643
Abhishek <abhi.ghosh27feb@gmail.com>
2021-04-17 12:12:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularbasics';
+  flag=false
+  myName='abhishek'
+  login(name,pass){
+
+  console.log("user name",name)
+  console.log("user pass",pass)
+  if(name==="abhishek" && pass==='123456'){
+    alert('hurrey')
+  }
+  else{
+
+    alert('login faiure')   
+  }
+  }
 }
